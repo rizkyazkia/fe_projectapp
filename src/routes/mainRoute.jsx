12b7f5be497c96c5d66988475980616be351d64c
rizkyazkia <72876433+rizkyazkia@users.noreply.github.com>
2026-07-01 01:23:40 +0700

    Add GamePage route to mainRoute

--- src/routes/mainRoute.jsx
+++ src/routes/mainRoute.jsx
@@ -1,11 +1,21 @@
 import { Navigate, Route } from "react-router-dom";
 import LandingPage from "../pages/main/LandingPage";
+import GamePage from "../pages/game/gamepage";
 
 const mainRoute = () => {
   return (
     <>
       <Route path="/" element={<Navigate to="/landing-page" replace />} />
-      <Route path="/landing-page" element={<LandingPage/>} />
+
+      <Route
+        path="/landing-page"
+        element={<LandingPage />}
+      />
+
+      <Route
+        path="/game"
+        element={<GamePage />}
+      />
     </>
   );
 };
